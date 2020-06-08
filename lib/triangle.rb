29936cb4b728071ff8f8877aa2a5bf8b66bd9bd9
Catheryne Small <catheryn.small@gmail.com>
2020-06-08 02:50:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,19 +7,7 @@ class Triangle
       @side_c = side_c
   end
   
-  def kind 
-    if @side_a <= 0.0 || @side_b <= 0.0 || @side_c <= 0.0
-      begin 
-        raise TriangleError
-      rescue TriangleError => error 
-          puts error.message
-      end
-    elsif @side_a == @side_b || @side_b == @side_c || @side_a == @side_c
-      @equilateral
-    elsif @side_a == @side_b 
-      
-    end
-  end
+
   
   class TriangleError < StandardError
     def message
