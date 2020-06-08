b28df01c8798eec592c321390a3a1300a753cd1b
Catheryne Small <catheryn.small@gmail.com>
2020-06-08 03:17:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,6 @@ class Triangle
   end
   
   def kind 
-    if @side_a <= 0.0 || @side_b <= 0.0 || @side_c <= 0.0
       if @side_a == @side_b && @side_a == @side_c 
         :equilateral
       elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
@@ -16,7 +15,6 @@ class Triangle
       else
         :scalene
       end
-    end
   end
   
   class TriangleError < StandardError
