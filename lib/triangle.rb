312e4faf2725f156c50f44894bf238261eb1c262
Catheryne Small <catheryn.small@gmail.com>
2020-06-08 03:14:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,8 +8,9 @@ class Triangle
   end
   
   def kind 
-    if (@side_a == @side_b && @side_a == @side_c) && (@side_a > 0.0 || @side_b > 0.0 || @side_c > 0.0) 
-        :equilateral
+    if @side_a <= 0.0 || @side_b <= 0.0 || @side_c <= 0.0 
+      @side_a == @side_b && @side_a == @side_c 
+      :equilateral
     elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
         :isosceles
     else
