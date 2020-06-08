dccdadee8dc477195d1b6c5d9b612a0b5be001a1
Catheryne Small <catheryn.small@gmail.com>
2020-06-08 01:36:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize(side_a, side_b, side_c)
+      @side_a = side_a
+      @side_b = side_b
+      @side_c = side_c
+  end
+  
+  def kind 
+    if @side_a > 0 && @side_b > 0 && @side_c > 0
+      if @side_a
+    end
+  end
 end
