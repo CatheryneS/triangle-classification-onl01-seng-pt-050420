8136309c95de012e137738a092de433a91f4a60d
Catheryne Small <catheryn.small@gmail.com>
2020-06-08 02:55:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,9 +13,9 @@ class Triangle
     end
   end
   
-  class TriangleError < StandardError
-    def message
+  #class TriangleError < StandardError
+   # def message
       "Length must be greater than zero."
-    end
-  end
+    #end
+  #end
 end
